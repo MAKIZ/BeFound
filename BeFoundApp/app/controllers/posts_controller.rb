e541945022c881d9252a8ebcd48249307652fdcf
Mark Delos Reyes <makizworks@gmail.com>
2017-09-11 20:48:28 -0400

    set post controller

--- BeFoundApp/app/controllers/posts_controller.rb
+++ BeFoundApp/app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name,
+                                 :photo,
+                                 :last_contact_date,
+                                 :sex,
+                                 :dob,
+                                 :complexion,
+                                 :description)
+  end
 end
