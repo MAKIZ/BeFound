ddc24144eb430f0c871ae7d7b7d8edac7d39e980
Mark Delos Reyes <makizworks@gmail.com>
2017-09-11 22:05:06 -0400

    users auth now working

--- BeFoundApp/config/routes.rb
+++ BeFoundApp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  root  "posts#index"
+  root to: 'sessions#new'
   resources :users, only: [:new, :create, :index, :show]
+  resource :session, only: [:new, :create, :destroy]
 end
--- BeFoundApp/db/schema.rb
+++ BeFoundApp/db/schema.rb
@@ -29,9 +29,12 @@ ActiveRecord::Schema.define(version: 20170912005758) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "session_token"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
