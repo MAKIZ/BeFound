047ca65656316278bc47a95b04f50ef59e3f7ef7
Mark Delos Reyes <makizworks@gmail.com>
2017-09-11 20:40:17 -0400

    migrate post db

--- BeFoundApp/db/migrate/20170912001423_create_posts.rb
+++ BeFoundApp/db/migrate/20170912001423_create_posts.rb
@@ -1,6 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
+      t.string :name
+      t.string :photo
+      t.string :last_contact_date
+      t.string :sex
+      t.string :dob
+      t.string :complexion
+      t.text :description
 
       t.timestamps
     end
